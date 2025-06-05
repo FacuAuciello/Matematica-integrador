29afc56201400170052eeb7c0405f5bff643d603
FacuAuciello <facundoauciello@gmail.com>
2025-06-05 17:04:53 -0300

    codigo completo

--- tp.py
+++ tp.py
@@ -2,10 +2,49 @@
 import time
 import random
 
-def generarTexto (cantidad):
+def generarTexto(cantidad):
     palabras = ["programacion", "python", "utn", "variables", "ayso", "oe", "matematica", "funciones"]
     resultado = []
     for i in range(cantidad):
         palabrasAzar = random.choice(palabras)
         resultado.append(palabrasAzar)
     return resultado
+
+#algoritmo con lista de tuplas
+def contarPalabrasLista(texto):
+    conteo = []
+    for palabra in texto:
+        encontrado = False
+        for i in range(len(conteo)):
+            if conteo[i][0] == palabra:
+                conteo[i] = (palabra, conteo[i][1] + 1)
+                encontrado = True
+            if not encontrado:
+                conteo.append((palabra, 1))
+    return conteo
+
+#algoritmo con diccionario
+def contarPalabrasDiccionario(texto):
+    conteo = {}
+    for palabra in texto:
+        if palabra in conteo:
+            conteo[palabra] += 1
+        else:
+            conteo[palabra] = 1
+    return conteo
+
+#medicion del tiempo de ejecucion/ expresamos en miliseg
+tamaniosEntrada = [1000, 5000, 10000, 50000, 100000]
+
+for numeros in tamaniosEntrada:
+    texto = generarTexto(numeros)
+
+    inicio = time.time()
+    contarPalabrasLista(texto)
+    fin = time.time()
+    print(f"Lista - {tamaniosEntrada} palabras: {round((fin - inicio) * 1000, 2)} ms") 
+
+    inicio = time.time()
+    contarPalabrasDiccionario(texto)
+    fin = time.time()
+    print(f"Diccionario - {tamaniosEntrada} palabras: {round((fin - inicio) * 1000, 2)} ms")
